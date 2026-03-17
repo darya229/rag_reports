eb7ab68a60554c9ca06618ecb97ba68e35ddd20d
darya229 <daryadariaa229@gmail.com>
2026-03-17 16:49:30 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ from retrieve import *
 
 deepseek_llm = ChatDeepSeek(
     model="deepseek-reasoner",
-    api_key = API_DEEPSEEK,
+    api_key = st.secrets["API_DEEPSEEK"],
     temperature=1,
     max_tokens=8000,
     reasoning_effort="medium",
@@ -31,7 +31,7 @@ def initialize_connections():
     try:
         client_db = QdrantClient(
             url = "http://176.109.105.181:6333/",
-            api_key=API_QDRANT
+            api_key=st.secrets["API_QDRANT"]
 )
         bm25_model = SparseTextEmbedding("Qdrant/bm25")
         dense_model = SentenceTransformer("Qwen/Qwen3-Embedding-0.6B")
