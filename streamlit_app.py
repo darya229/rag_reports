4d5c95546984d657496c6ef46d8c209906ead3f5
darya229 <daryadariaa229@gmail.com>
2026-04-06 15:33:10 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -351,15 +351,15 @@ hide_streamlit_style = """
                 height: 0%;
                 position: fixed;
                 }
-                div[data-testid="stDecoration"] {
-                visibility: hidden;
-                height: 0%;
-                position: fixed;
-                }
-                #MainMenu {
-                visibility: hidden;
-                height: 0%;
-                }
+                # div[data-testid="stDecoration"] {
+                # visibility: hidden;
+                # height: 0%;
+                # position: fixed;
+                # }
+                # #MainMenu {
+                # visibility: hidden;
+                # height: 0%;
+                # }
 
                 </style>
                 """
